072e59f44f7e15aa541d4e2e22ddc70616081f45
AADITYA MANOJ <aadityamanoj2007@gmail.com>
2026-04-26 12:27:03 +0530

    small tweeks

--- backend/main.py
+++ backend/main.py
@@ -28,7 +28,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://192.168.137.1:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://helprag-app-413bf.web.app",
+        "https://helprag-app-413bf.firebaseapp.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
